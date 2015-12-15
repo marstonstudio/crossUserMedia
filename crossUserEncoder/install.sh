2e23ac253b1fc7c78d68588fd63d039fca80a6a2
Jon Marston <jon.marston@englishcentral.com>
2015-12-15 16:17:35 -0500

    updated ffmpeg

--- crossUserEncoder/install.sh
+++ crossUserEncoder/install.sh
@@ -3,7 +3,9 @@
 rm -Rf out
 mkdir out
 cd ffmpeg
-./configure  --prefix=../out --disable-ffplay --disable-ffprobe --disable-ffserver
+./configure  --prefix=../out --disable-ffplay --disable-ffprobe --disable-ffserver --disable-everything \
+    --enable-encoder=aac --enable-decoder=aac --enable-encoder=pcm_s16le --enable-decoder=pcm_s16le \
+    --enable-muxer=mp4 --enable-demuxer=mp4 --enable-muxer=wav --enable-demuxer=wav
 make && make install
 make clean
 
