c828dc25c08abf6400a3bebccbe9e563193152a6
Jon Marston <jon.marston@englishcentral.com>
2015-12-16 10:01:45 -0500

    move ffmpeg to be a submodule

--- crossUserEncoder/gcc-build.sh
+++ crossUserEncoder/gcc-build.sh
@@ -5,22 +5,59 @@ mkdir dist
 cd ffmpeg
 make clean
 
-./configure  --prefix=../dist \
-    --disable-ffplay --disable-ffprobe --disable-ffserver \
-    --disable-doc --disable-iconv --disable-sdl --disable-securetransport \
-    --disable-runtime-cpudetect --disable-asm \
-    --disable-d3d11va --disable-dxva2 --disable-vaapi --disable-vda --disable-vdpau \
-    --disable-encoders --enable-encoder=aac \
-    --disable-decoders --enable-decoder=pcm_s16le \
+./configure \
+    --prefix=../dist \
+\
+    --disable-runtime-cpudetect \
+\
+    --disable-ffplay \
+    --disable-ffprobe \
+    --disable-ffserver \
+\
+    --disable-doc \
+\
+    --disable-pthreads \
+    --disable-w32threads \
+    --disable-os2threads \
+\
+    --disable-d3d11va \
+    --disable-dxva2 \
+    --disable-vaapi \
+    --disable-vda \
+    --disable-vdpau \
+\
+    --disable-encoders \
+    --enable-encoder=aac \
+    --disable-decoders \
+    --enable-decoder=pcm_s16le \
     --disable-hwaccels \
-    --disable-muxers --enable-muxer=mp4 \
-    --disable-demuxers --enable-demuxer=wav \
+    --disable-muxers \
+    --enable-muxer=mp4 \
+    --disable-demuxers \
+    --enable-demuxer=wav \
     --disable-parsers \
     --disable-bsfs \
-    --disable-protocols --enable-protocol=file \
+    --disable-protocols \
+    --enable-protocol=file \
     --disable-indevs \
     --disable-outdevs \
-    --disable-filters --enable-filter=aresample
+    --disable-filters \
+    --enable-filter=aresample \
+\
+    --disable-iconv \
+    --disable-sdl \
+    --disable-securetransport \
+    --disable-xlib \
+\
+    --arch=x86_32 \
+    --cpu=generic \
+    --target-os=none \
+\
+    --disable-asm \
+    --disable-fast-unaligned \
+\
+    --disable-debug \
+    --disable-stripping
 
 make && make install
 make clean
