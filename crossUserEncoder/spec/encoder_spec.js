df7cc7ede373ac83dd6c92cf6a387bbdb6674849
Jon Marston <jon.marston@englishcentral.com>
2016-01-09 14:13:32 -0500

    working jasmine test

--- crossUserEncoder/spec/encoder_spec.js
+++ crossUserEncoder/spec/encoder_spec.js
@@ -1,20 +1,14 @@
 describe("Encoder", function() {
 
-    var NodeFS = require('fs');
-    var inputWav = NodeFS.readFileSync('spec/input.wav');   //relative to location where you run npm test
-    var inputUint8 = new Uint8Array(inputWav);
-    console.log('inputUint8.length: ' + inputUint8.length);
-
     var AACEncoder = require('../dist/index.js');    //relative to spec file
 
-    //var wavBlob = new Blob([ wavBuffer ], { type: 'audio/wav' });
-    //console.log('wavBlob.size' + wavBlob.size);
-
-    it("should test here", function() {
+    it("test encoding", function() {
+        var NodeFS = require('fs');
+        var inputWav = NodeFS.readFileSync('spec/input.wav');   //relative to location where you run npm test
+        var inputUint8 = new Uint8Array(inputWav);
+        console.log('inputUint8.length: ' + inputUint8.length);
 
         var aacEncoder = new AACEncoder('32k', 'input.wav', inputUint8);
-
-        console.log("inside it");
     });
 
 });
--- crossUserEncoder/src/interface.js
+++ crossUserEncoder/src/interface.js
@@ -3,25 +3,34 @@ function AACEncoder(bitrate, inputName, inputData) {
     var _encodedOutput;
     var _bitrate = bitrate;
     var _inputData = inputData;
-    var _inputName = inputName;
+    var _inputName = '/data/' + inputName;
+    var _outputName = '/data/output.mp4';
 
     var Module = {};
 
-    Module['arguments'] = [
-        '-version'
-    ];
+    Module['print'] = function(text) { console.log('stdout: ' + text); };
+
+    Module['printErr'] = function(text) { console.error('stderr: ' + text); };
 
     Module['preRun'] = function() {
-        console.log('preRun');
-
         FS.mkdir('/data');
-        FS.chdir('/data');
 
         var inputFile = FS.open(_inputName, "w+");
         FS.write(inputFile, _inputData, 0, _inputData.length);
         FS.close(inputFile);
     };
 
+    Module['arguments'] = [
+        '-i', _inputName,
+        '-b:a', _bitrate,
+        '-strict', '-2',
+        '-nostdin',
+        _outputName
+    ];
+
+    Module['postRun'] = function() {
+        console.log('postRun');
+    }
 
     /*EMSCRIPTENBODY*/
 
