313aecaf0d409da18898b07d0fc7ee865b2ffaec
Jon Marston <jon.marston@englishcentral.com>
2016-04-05 17:21:33 -0400

    use string and array input methods to emscripten

--- encoder/js/post.js
+++ encoder/js/post.js
@@ -30,21 +30,12 @@ this.onerror = function(e) {
 var init = function(inputFormat, inputSampleRate, outputFormat, outputSampleRate, outputBitRate) {
     console.log('encoder.js init inputFormat:' + inputFormat + ', inputSampleRate:' + inputSampleRate + ', outputFormat:' + outputFormat + ', outputSampleRate:' + outputSampleRate + ', outputBitRate:' + outputBitRate);
 
-    var inputFormatBuffer = Module._malloc(inputFormat.length+1);
-    Module.writeStringToMemory(inputFormat, inputFormatBuffer);
-
-    var outputFormatBuffer = Module._malloc(outputFormat.length+1);
-    Module.writeStringToMemory(outputFormat, outputFormatBuffer);
-
     Module.ccall(
         'init',
         null,
-        ['number', 'number', 'number', 'number', 'number'],
-        [inputFormatBuffer, inputSampleRate, outputFormatBuffer, outputSampleRate,  outputBitRate]
+        ['string', 'number', 'string', 'number', 'number'],
+        [inputFormat, inputSampleRate, outputFormat, outputSampleRate,  outputBitRate]
     );
-
-    Module._free(inputFormatBuffer);
-    Module._free(outputFormatBuffer);
 
     self.postMessage({'cmd':'initComplete'});
 };
@@ -53,16 +44,12 @@ var load = function(inputAudio) {
     console.log('encoder.js load inputAudio.byteLength:' + inputAudio.byteLength);
 
     var inputAudioArray = new Uint8Array(inputAudio);
-    var inputAudioBuffer = Module._malloc(inputAudioArray.length);
-    Module.writeArrayToMemory(inputAudioArray, inputAudioBuffer);
     Module.ccall(
         'load',
         null,
-        ['number', 'number'],
-        [inputAudioBuffer, inputAudioArray.length]
+        ['array', 'number'],
+        [inputAudioArray, inputAudioArray.length]
     );
-
-    Module._free(inputAudioBuffer);
 
     self.postMessage({'cmd':'loadComplete'});
 };
