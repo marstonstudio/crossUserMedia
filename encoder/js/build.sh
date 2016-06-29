797d62108d6b867b688f0fe5c341cff2dd3c8ec3
Jon Marston <jon.marston@englishcentral.com>
2016-06-29 13:29:02 -0400

    fix build.sh for emscripten

--- encoder/js/build.sh
+++ encoder/js/build.sh
@@ -19,7 +19,7 @@ fi
 #EOF
 
 echo "Starting ffmpeg configuration"
-emconfigure /usr/bin/sh ./configure \
+emconfigure ./configure \
     --prefix=../js/dist \
 \
     --disable-runtime-cpudetect \
@@ -43,13 +43,13 @@ emconfigure /usr/bin/sh ./configure \
 \
     --disable-encoders \
     --enable-encoder=aac \
-    --enable-encoder=pcm_f32be `#Enable pcm_f32be for the passthru` \    
+    --enable-encoder=pcm_f32be \
     --disable-decoders \
     --enable-decoder=pcm_f32le \
     --disable-hwaccels \
     --disable-muxers \
     --enable-muxer=mp4 \
-    --enable-muxer=pcm_f32be `#Enable pcm_f32be for the passthru` \    
+    --enable-muxer=pcm_f32be \
     --disable-demuxers \
     --enable-demuxer=pcm_f32le \
     --disable-parsers \
